5942e3d6dcf2bdda958eb597099ba888bebfd7ea
nigelhughesfairsailcom <nigel.hughes@sage.com>
2021-03-26 09:27:08 +0000

    Update org_config.py

--- org_config.py
+++ org_config.py
@@ -13,7 +13,7 @@ SCRATCH_DEF = "config/project-scratch-def.json"
 DURATION = 10
 
 # Default Devhub
-DEVHUB = "my-dev-hub-org"
+DEVHUB = "my-hub-org"
 
 # List of managed package Ids to install into the Org.
 PACKAGE_IDS = []
@@ -27,13 +27,17 @@ PRE_DEPLOY = []
 # List of metadata source folders (SRC_FOLDERS = ["force-app"])
 SRC_FOLDERS = []
 
-# Anonymous APEX file to execute ("setupdata.apex")
-BUILD_DATA_CMD = ""
-
 # List of permission sets to assign to the user.
 P_SETS = []
 
-# Post-Deploy
-# Deploy the profile again as does not work
-# until the community has been created.
+# Anonymous APEX files to execute ("setupdata.apex")
+BUILD_DATA_CMD = []
+
+# Name of template to use to create the community
+TMPLT_NAME = None
+
+# Name of the Lightning community that you want to publish.
+SITE_NAME = None
+
+# Post-Deploy use if metadata deploy sequence is important.
 POST_DEPLOY = []
