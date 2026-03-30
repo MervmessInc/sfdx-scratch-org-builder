dfed096e03e36baf7ba7cee239121ff141c3900d
Nigel Hughes <merv@merv.org.uk>
2026-03-30 11:33:15 +0100

    release: v0.1.0 - modernise packaging and runtime

--- sf_org_manager/__init__.py
+++ sf_org_manager/__init__.py
@@ -3,6 +3,6 @@ from importlib.metadata import PackageNotFoundError, version
 try:
     __version__ = version("sf-org-manager")
 except PackageNotFoundError:
-    __version__ = "0.0.0.dev"
+    __version__ = "0.1.0.dev"
 
 
